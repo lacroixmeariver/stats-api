0e86dae2d0604c6f71afe9d6bbd6fde5062bbad1
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-07-07 18:05:27 -0700

    Updating schema

--- schema.sql
+++ schema.sql
@@ -10,10 +10,12 @@ CREATE TABLE IF NOT EXISTS players (
     country VARCHAR(50) NOT NULL,
     draft_year INTEGER,
     draft_round INTEGER,
-    draft_number INTEGER
+    draft_number INTEGER,
+    team_id INTEGER, 
+    FOREIGN KEY (team_id) REFERENCES teams(id)
 );
 
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS teams (
     id INTEGER PRIMARY KEY,
     conference VARCHAR(15),
     division VARCHAR(15),
@@ -23,11 +25,11 @@ CREATE TABLE IF NOT EXISTS (
     abbreviation VARCHAR(15)
 );
 
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS games (
     id INTEGER PRIMARY KEY,
     date VARCHAR(20),
     season INTEGER,
-    status VARCHAR(50),
+    status VARCHAR(100),
     period INTEGER,
     time VARCHAR(50),
     postseason BOOLEAN,
@@ -39,4 +41,23 @@ CREATE TABLE IF NOT EXISTS (
     home_q2 INTEGER DEFAULT 0,
     home_q3 INTEGER DEFAULT 0,
     home_q4 INTEGER DEFAULT 0,
+    home_ot1 INTEGER,
+    home_ot2 INTEGER,
+    home_ot3 INTEGER,
+    home_timeouts_remaining INTEGER,
+    home_in_bonus BOOLEAN,
+    visitor_q1 INTEGER DEFAULT 0,
+    visitor_q2 INTEGER DEFAULT 0,
+    visitor_q3 INTEGER DEFAULT 0,
+    visitor_q4 INTEGER DEFAULT 0,
+    visitor_ot1 INTEGER,
+    visitor_ot2 INTEGER,
+    visitor_ot3 INTEGER,
+    visitor_timeouts_remaining INTEGER,
+    visitor_in_bonus BOOLEAN,
+    ist_stage VARCHAR(50),
+    home_team_id INTEGER,
+    visitor_team_score INTEGER,
+    FOREIGN KEY (home_team_id) REFERENCES teams(id),
+    FOREIGN KEY (visitor_team_id) REFERENCES teams(id)
 );
