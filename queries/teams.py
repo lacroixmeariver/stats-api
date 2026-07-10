dd517f82bab1983eacc66ee7ac52fb16a5519a18
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-07-10 11:12:03 -0700

    Adjusting team queries response with RealDictCursor

--- queries/teams.py
+++ queries/teams.py
@@ -1,4 +1,5 @@
 from database import pool
+from psycopg2.extras import RealDictCursor
 
 def upsert_all_teams(all_teams_data):
     conn = pool.getconn()
@@ -37,3 +38,16 @@ def upsert_team(team):
     conn.commit()
     cur.close()
     pool.putconn(conn)
+
+def get_all_teams():
+    conn = pool.getconn()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    # RealDictCursor allows for the data returned from db to be accessed according to attributes
+    cur.execute(
+        """ SELECT id, conference, division, city, name, full_name, abbreviation FROM teams; """    
+    )
+    result = cur.fetchall()
+    cur.close()
+    pool.putconn(conn)
+    return result
+    
