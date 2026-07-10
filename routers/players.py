b0f4d7b79e51373191bede0eac306916fbeb93a3
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-07-10 11:25:14 -0700

    Small fixes in import statements for refactored db query routes

--- routers/players.py
+++ routers/players.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
-from services import balldontlie as api_service
+#from ingestion import client as api_service
+from queries.players import get_all_players as db_get_players
 router = APIRouter()
 
 @router.get("/")
 async def get_players():
-    return api_service.get_all_players()
+    return db_get_players()
 
 @router.get("/{player_id}")
 async def get_player(player_id: int):
-    return api_service.get_player(player_id)
+    # return api_service.get_player(player_id)
+    return
+    # TODO Add specific player db query
 
--- routers/teams.py
+++ routers/teams.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from services import balldontlie as api_service
+#from services import balldontlie as api_service
+from queries.teams import get_all_teams as db_all_teams
 router = APIRouter()
 
 @router.get("/")
-async def get_teams():
-    return api_service.get_all_teams()
+async def get_all_teams():
+    #return api_service.get_all_teams()
+    return db_all_teams()
