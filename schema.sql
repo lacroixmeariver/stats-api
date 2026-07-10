0381335e1bf55bb62004168059a24cc7c54a21c6
Ingrid Llorente <143571288+lacroixmeariver@users.noreply.github.com>
2026-07-10 11:08:48 -0700

    Updating schema for individual games table to reflect data available on current subscription tier

--- schema.sql
+++ schema.sql
@@ -25,8 +25,6 @@ CREATE TABLE IF NOT EXISTS players (
     FOREIGN KEY (team_id) REFERENCES teams(id)
 );
 
-
-
 CREATE TABLE IF NOT EXISTS games (
     id INTEGER PRIMARY KEY,
     date VARCHAR(20),
@@ -35,29 +33,8 @@ CREATE TABLE IF NOT EXISTS games (
     period INTEGER,
     time VARCHAR(50),
     postseason BOOLEAN,
-    postponed BOOLEAN,
     home_team_score INTEGER,
     visitor_team_score INTEGER,
-    datetime VARCHAR(100),
-    home_q1 INTEGER DEFAULT 0,
-    home_q2 INTEGER DEFAULT 0,
-    home_q3 INTEGER DEFAULT 0,
-    home_q4 INTEGER DEFAULT 0,
-    home_ot1 INTEGER,
-    home_ot2 INTEGER,
-    home_ot3 INTEGER,
-    home_timeouts_remaining INTEGER,
-    home_in_bonus BOOLEAN,
-    visitor_q1 INTEGER DEFAULT 0,
-    visitor_q2 INTEGER DEFAULT 0,
-    visitor_q3 INTEGER DEFAULT 0,
-    visitor_q4 INTEGER DEFAULT 0,
-    visitor_ot1 INTEGER,
-    visitor_ot2 INTEGER,
-    visitor_ot3 INTEGER,
-    visitor_timeouts_remaining INTEGER,
-    visitor_in_bonus BOOLEAN,
-    ist_stage VARCHAR(50),
     home_team_id INTEGER,
     visitor_team_id INTEGER,
     FOREIGN KEY (home_team_id) REFERENCES teams(id),
